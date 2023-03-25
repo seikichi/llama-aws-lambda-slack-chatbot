71558571b3bbefabea961cdfbb01c5428240ff69
Seikichi KONDO <seikichi@kmc.gr.jp>
2023-03-25 23:28:40 +0900

    Initial commit

---
+++ test/llama-aws-lambda-slack-chatbot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LlamaAwsLambdaSlackChatbot from '../lib/llama-aws-lambda-slack-chatbot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/llama-aws-lambda-slack-chatbot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LlamaAwsLambdaSlackChatbot.LlamaAwsLambdaSlackChatbotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
